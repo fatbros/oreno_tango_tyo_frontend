632801b970e36149f12744f600377cc554b14e12
shun kondo <02luvsic@gmail.com>
2018-06-14 14:21:48 +0900

    add next

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+class Index extends React.Component {
+  render() {
+    return <p>oreno tango tyo application</p>
+  }
+}
+
+export default connect()(Index)
