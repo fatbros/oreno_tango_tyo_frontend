d68291b8c65aa8d56ab7aca6f2d1f9c9961b0794
shun kondo <02luvsic@gmail.com>
2018-06-19 12:22:44 +0900

    add ajax fail pattern

--- saga/twitter.js
+++ saga/twitter.js
@@ -7,30 +7,48 @@ import { TWITTER } from '../constants/twitter'
 import { setRequestToken, setAccessToken } from '../actions/twitter'
 
 const getRequestTokenToAPI = async () => {
-  const res = await axios.post('/api/v1/auth/twitter_request_token')
-  return res.data
+  try {
+    const res = await axios.post('/api/v1/auth/twitter_request_tokenaa')
+    return res.data
+  } catch (err) {
+    throw err
+  }
 }
 
 export function* getRequestToken() {
   while (true) {
-    yield take(TWITTER.GET_REQUEST_TOKEN)
-    const requestToken = yield call(getRequestTokenToAPI)
+    try {
+      yield take(TWITTER.GET_REQUEST_TOKEN)
+      const requestToken = yield call(getRequestTokenToAPI)
 
-    yield put(setRequestToken(requestToken))
-    window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${requestToken}`
+      yield put(setRequestToken(requestToken))
+      window.location.href = `https://api.twitter.com/oauth/authenticate?oauth_token=${requestToken}`
+    } catch (err) {
+      // 通信失敗
+      console.warn(err)
+    }
   }
 }
 
 const getAccessTokenToAPI = async params => {
-  const res = await axios.post('/api/v1/auth/twitter_access_token', params)
-  return res.data
+  try {
+    const res = await axios.post('/api/v1/auth/twitter_access_token', params)
+    return res.data
+  } catch (err) {
+    throw err
+  }
 }
 
 export function* getAccessToken() {
   while (true) {
-    const takeAction = yield take(TWITTER.GET_ACCESS_TOKEN)
-    const accessToken = yield call(getAccessTokenToAPI, takeAction.payload)
-    yield put(setAccessToken(accessToken))
-    Router.push('/')
+    try {
+      const takeAction = yield take(TWITTER.GET_ACCESS_TOKEN)
+      const accessToken = yield call(getAccessTokenToAPI, takeAction.payload)
+      yield put(setAccessToken(accessToken))
+      Router.push('/')
+    } catch (err) {
+      // 通信失敗
+      console.warn(err)
+    }
   }
 }
